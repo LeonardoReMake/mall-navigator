89d763bee1383f1d9fca3e62e52a7608aff3e606
Leonid Smirnov <remakeleonardo@gmail.com>
2017-11-23 21:46:09 +0300

    Create idea project

---
+++ src/test/java/ru/navigator/mall_navigator/MainTest.java
@@ -0,0 +1,19 @@
+package ru.navigator.mall-navigator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration( locations={"classpath:applicationContext.xml"} )
+public class MainTest {
+    private static final Logger LOG = LoggerFactory.getLogger(MainTest.class);
+
+    @Test
+    public void runTest() {
+        LOG.info("Test");
+    }
+}
